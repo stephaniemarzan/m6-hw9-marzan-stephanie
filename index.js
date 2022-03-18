c6534b75eb02970816d014f908f3fa78308137e2
Stephanie Marzan <s.marzanoberholz@ufl.edu>
2022-03-18 18:13:49 -0500

    added clouds and temperature with js

---
+++ index.js
@@ -0,0 +1,38 @@
+var weatherWidget = document.getElementById('weather-widget')
+var form = document.querySelector('form')
+var cityLocation = document.querySelector('input[type=text]')
+
+form.onsubmit = function(e){
+    e.preventDefault()
+    console.log(cityLocation.value)
+    fetch('https://api.openweathermap.org/data/2.5/weather?appid=aba511288f68d80348241c0fad51e601&units=imperial&q=' + cityLocation.value)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+
+        var div = document.createElement('div')
+        var h3 = document.createElement('h3')
+        h3.textContent = data.name + ", " + data.sys.country
+        div.appendChild(h3)
+
+        var p = document.createElement('p')
+        p.textContent = data.weather[0].description
+        div.appendChild(p)
+
+        var p2 = document.createElement('p')
+        p2.textContent = "Current: "+ data.main.temp + "\u00B0 F"
+        div.appendChild(p2)
+
+        var p3 = document.createElement('p')
+        p3.textContent = "Feels Like: "+ data.main.feels_like+ "\u00B0 F"
+        div.appendChild(p3)
+
+        weatherWidget.appendChild(div)
+    })
+    .catch(function(err){
+        console.log(err)
+    })
+}
+
